209fff4a5cf383cbb12d2fa375f5354d0196f9cd
purva5194 <purvapatel@Purvas-MacBook-Pro.local>
2017-12-02 12:27:49 -0800

    Added Signin API

---
+++ nodeserver/nodeserver.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+var mongojs = require('mongojs');
+var db = mongojs('user:user@ds127436.mlab.com:27436/gogreendatabase',['userlist']);
+var bodyParser = require('body-parser');
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+
+
+//get user details
+app.get('/userlist/:password/:emailid', function (req, res) {
+  console.log('I received a GET request');
+  db.userlist.find({"password":req.params.password, "emailid":req.params.emailid}, function (err, docs) {
+    console.log(docs);
+    res.json(docs);
+  });
+});
+
+//insert user details
+app.post('/userlist', function (req, res) {
+  console.log(req.body);
+  db.userlist.insert(req.body, function(err, doc) {
+    res.json({"success" : "1"});
+  });
+});
+
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
