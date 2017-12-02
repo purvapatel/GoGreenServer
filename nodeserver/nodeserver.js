c6abf8fc79b00003dfc676136a993a6e1728558f
purva5194 <purvapatel@Purvas-MacBook-Pro.local>
2017-12-02 14:21:52 -0800

    Changes in Get User-list API

--- nodeserver/nodeserver.js
+++ nodeserver/nodeserver.js
@@ -1,17 +1,26 @@
 var express = require('express');
 var app = express();
 var mongojs = require('mongojs');
-var db = mongojs('user:user@ds127436.mlab.com:27436/gogreendatabase',['userlist']);
+var db = mongojs('user:user@ds227555.mlab.com:27555/mobileuserdb',['UserDetails']);
 var bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 
+//get list of users
+app.get('/userlist', function (req, res) {
+  console.log('I received a GET request');
+  db.UserDetails.find({}, function (err, docs) {
+    console.log(docs);
+    res.json(docs);
+  });
+});
 
 //get user details
-app.get('/userlist/:password/:emailid', function (req, res) {
+app.get('/userlist/:name', function (req, res) {
   console.log('I received a GET request');
-  db.userlist.find({"password":req.params.password, "emailid":req.params.emailid}, function (err, docs) {
+	var name = req.params.name;
+  db.UserDetails.find({"name" : req.params.name}, function (err, docs) {
     console.log(docs);
     res.json(docs);
   });
@@ -20,11 +29,19 @@ app.get('/userlist/:password/:emailid', function (req, res) {
 //insert user details
 app.post('/userlist', function (req, res) {
   console.log(req.body);
-  db.userlist.insert(req.body, function(err, doc) {
+  db.UserDetails.insert(req.body, function(err, doc) {
     res.json({"success" : "1"});
   });
 });
 
+
+//insert user profile
+app.post('/userprofile', function (req, res) {
+  console.log(req.body);
+  db.UserProfile.insert(req.body, function(err, doc) {
+    res.json({"success" : "1"});
+  });
+});
 
 app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
