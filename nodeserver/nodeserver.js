41332ff6296432f111d528972826a8e49c3dceb3
Amrapali Sarkar <amrapali.sarkar@sjsu.edu>
2017-12-02 18:00:01 -0800

    Added APIs for User list

--- nodeserver/nodeserver.js
+++ nodeserver/nodeserver.js
@@ -8,10 +8,37 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 
 
-//get user details
+//get all user details
+app.get('/allUserlist', function (req, res) {
+  console.log('I received a GET request for all user details');
+  db.userlist.find({}, function (err, docs) {
+    console.log(docs);
+    res.json(docs);
+  });
+});
+
+//get user details for authentication
 app.get('/userlist/:email/:password', function (req, res) {
-  console.log('I received a GET request');
+  console.log('I received a GET request for authentication');
   db.userlist.find({"email":req.params.email, "password":req.params.password}, function (err, docs) {
+    console.log(docs);
+    res.json(docs);
+  });
+});
+
+//get user details for user display
+app.get('/userlist/:name', function (req, res) {
+  console.log('I received a GET request');
+  db.userlist.find({"name":req.params.name},{"name" : 1,"email" : 1, "mobile" : 1}, function (err, docs) {
+    console.log(docs);
+    res.json(docs);
+  });
+});
+
+//get user list with name and id
+app.get('/userlist', function (req, res) {
+  console.log('I received a GET request');
+  db.userlist.find({},{"name" : 1,"_id" : 1}, function (err, docs) {
     console.log(docs);
     res.json(docs);
   });
@@ -21,6 +48,14 @@ app.get('/userlist/:email/:password', function (req, res) {
 app.post('/userlist', function (req, res) {
   console.log(req.body);
   db.userlist.insert(req.body, function(err, doc) {
+    res.json({"success" : "1"});
+  });
+});
+
+//insert user details
+app.delete('/userlist/:name', function (req, res) {
+  console.log(req.body);
+  db.userlist.remove({name: req.params.name}, function(err, doc) {
     res.json({"success" : "1"});
   });
 });
